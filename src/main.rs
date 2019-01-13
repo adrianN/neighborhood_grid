0cbea5bf9fcf6b5d356db9ef5d81fdb3a86895fa
Adrian Neumann <adrian_neumann@gmx.de>
2019-01-13 10:39:40 +0100

    run 16 threads in parallel

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 extern crate permutohedron;
 extern crate rand;
 use permutohedron::Heap;
-use permutohedron::factorial;
 use rand::seq::SliceRandom;
+use std::cmp::min;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 const NUMBER_OF_ELEMENTS: usize = 4;
 
-fn fits(v: &[usize], pos: usize, matrix: &mut [usize], spot : usize) -> bool {
+fn fits(prefix: &[usize], v: &[usize], pos: usize, matrix: &mut [usize], spot: usize) -> bool {
     if spot == 0 {
         return true;
     }
@@ -19,24 +21,41 @@ fn fits(v: &[usize], pos: usize, matrix: &mut [usize], spot : usize) -> bool {
     let column_ok = if spot < NUMBER_OF_ELEMENTS {
         true
     } else {
-        v[pos] > v[matrix[spot - NUMBER_OF_ELEMENTS]]
+        let m_pos = matrix[spot - NUMBER_OF_ELEMENTS];
+        let l_value = if pos >= prefix.len() {
+            v[pos - prefix.len()]
+        } else {
+            prefix[pos]
+        };
+        let r_value = if m_pos >= prefix.len() {
+            v[m_pos - prefix.len()]
+        } else {
+            prefix[m_pos]
+        };
+        l_value > r_value
     };
     row_ok && column_ok
 }
 
-
-fn fill_rec(v: &[usize], chosen: &mut [bool], matrix: &mut [usize], cutoff : u64, spot : usize) -> u64 {
+fn fill_rec(
+    prefix: &[usize],
+    v: &[usize],
+    chosen: &mut [bool],
+    matrix: &mut [usize],
+    cutoff: u64,
+    spot: usize,
+) -> u64 {
     let mut count: u64 = 0;
     let mut done = 0;
-    for i in 0..v.len() {
+    for i in 0..(prefix.len() + v.len()) {
         if chosen[i] {
             done += 1;
             continue;
         }
-        if fits(v, i, matrix, spot) {
+        if fits(prefix, v, i, matrix, spot) {
             chosen[i] = true;
             matrix[spot] = i;
-            count = count.saturating_add(fill_rec(v, chosen, matrix, cutoff, spot+1));
+            count = count.saturating_add(fill_rec(prefix, v, chosen, matrix, cutoff, spot + 1));
             if count > cutoff {
                 return u64::max_value();
             }
@@ -44,51 +63,72 @@ fn fill_rec(v: &[usize], chosen: &mut [bool], matrix: &mut [usize], cutoff : u64
         }
     }
     if done == chosen.len() {
-       1
+        1
     } else {
         count
     }
 }
 fn main() {
-    let mut v: [usize; NUMBER_OF_ELEMENTS*NUMBER_OF_ELEMENTS] = [0; NUMBER_OF_ELEMENTS*NUMBER_OF_ELEMENTS];
+    let mut v: [usize; NUMBER_OF_ELEMENTS * NUMBER_OF_ELEMENTS] =
+        [0; NUMBER_OF_ELEMENTS * NUMBER_OF_ELEMENTS];
     for i in 0..NUMBER_OF_ELEMENTS * NUMBER_OF_ELEMENTS {
         v[i] = i;
     }
     println!("{:?}", v);
-    let mut chosen : [bool; NUMBER_OF_ELEMENTS*NUMBER_OF_ELEMENTS] = [false; NUMBER_OF_ELEMENTS*NUMBER_OF_ELEMENTS];
-    let mut matrix: [usize; NUMBER_OF_ELEMENTS*NUMBER_OF_ELEMENTS] = [0; NUMBER_OF_ELEMENTS*NUMBER_OF_ELEMENTS];
+    let mut matrix: [usize; NUMBER_OF_ELEMENTS * NUMBER_OF_ELEMENTS] =
+        [0; NUMBER_OF_ELEMENTS * NUMBER_OF_ELEMENTS];
     let mut m = u64::max_value();
-    let mut rng = rand::thread_rng();
-    for _i in 0..10000 {
-        v.shuffle(&mut rng);
-        let new_m = fill_rec(&v, &mut chosen, &mut matrix, m, 0);
-        for i in 0..chosen.len() {
-            chosen[i] = false;
-        }
-        if new_m <= m {
-            m = new_m;
-            println!("Permutation {:?}, count {}", v, m);
+    {
+        let mut chosen = [false; NUMBER_OF_ELEMENTS * NUMBER_OF_ELEMENTS];
+        let mut rng = rand::thread_rng();
+        let prefix = [];
+        for _i in 0..1000000 {
+            v.shuffle(&mut rng);
+            let new_m = fill_rec(&prefix, &v, &mut chosen, &mut matrix, m, 0);
+            for i in 0..chosen.len() {
+                chosen[i] = false;
+            }
+            if new_m <= m {
+                m = new_m;
+                //println!("Permutation {:?}, count {}", v, m);
+            }
         }
     }
+    let cutoff = Arc::new(Mutex::new(m));
 
-    let heap = Heap::new(&mut v);
-    let num_permutations = factorial(NUMBER_OF_ELEMENTS*NUMBER_OF_ELEMENTS)/100;
-    let mut count = 0;
-    let mut percent = 0;
-    for p in heap {
-        count += 1;
-        if count > num_permutations {
-            percent += 1;
-            println!("{}% done!", percent);
-            count = 0;
-        }
-        let new_m = fill_rec(&p, &mut chosen, &mut matrix, m, 0);
-        for i in 0..chosen.len() {
-            chosen[i] = false;
-        }
-        if new_m <= m {
-            m = new_m;
-            println!("Permutation {:?}, count {}", p, m);
-        }
+    let mut threads = Vec::new();
+    for _i in 0..v.len() {
+        v.rotate_left(1);
+        let cutoff = cutoff.clone();
+        let mut u = v.clone();
+        threads.push(thread::spawn(move || {
+            let mut m = u64::max_value();
+            let prefix = [u[0]];
+            let mut chosen = [false; NUMBER_OF_ELEMENTS * NUMBER_OF_ELEMENTS];
+            let heap = Heap::new(&mut u[1..]);
+
+            let mut count = 0;
+            for p in heap {
+                count += 1;
+                if count > 1000000 {
+                    count = 0;
+                    m = min(m, *cutoff.lock().unwrap());
+                }
+                let new_m = fill_rec(&prefix, &p, &mut chosen, &mut matrix, m, 0);
+                for i in 0..chosen.len() {
+                    chosen[i] = false;
+                }
+                if new_m <= m {
+                    m = new_m;
+                    println!("Permutation {:?}, count {}", p, m);
+                    let mut c = cutoff.lock().unwrap();
+                    *c = min(*c, m);
+                    m = *c;
+                }
+            }
+        }));
+    }
+    for t in threads {
+        t.join().unwrap();
     }
 }
